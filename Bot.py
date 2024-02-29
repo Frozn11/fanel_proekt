7c2db4e683164c1ebac0aab4b54ad367a786b6f9
Frozn_11 <karim.elkhawankey@gmail.com>
2024-02-29 11:57:42 +0300

    new

--- Bot.py
+++ Bot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
-from setting import TOKEN
+from setting import TOKEN,appid
 import requests
+import time
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,55 +14,145 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+            
+class SimpleView(discord.ui.View):
+        foo : bool = None
+
+        async def disable_all_items(self):
+              for item in self.children:
+                  item.disabled = True
+              await self.message.edit(view=self)
+        async def on_timeout(self) -> None:
+              await self.message.channel.send("TimeOut")
+              await self.disable_all_items()
+        
+        @discord.ui.button(label="weather now", 
+                       style=discord.ButtonStyle.success)
+        async def weathernow(self, interaction: discord.Interaction, button: discord.ui.Button):
+
+            # msg = await interaction.response.send_message("https://cdn.discordapp.com/emojis/1138172643867111595.gif?size=96&quality=lossless")
+            emojes = {
+                "пасмурно": [":cloud:"],
+                "ясно": [":sunny:"],
+                "облачно с прояснениями": [":white_sun_cloud: "],
+                "дождь": [":cloud_rain:"],
+                "переменная облачность": [":white_sun_small_cloud: "]
+
+            }
+            city_id = 0
+            res = requests.get("http://api.openweathermap.org/data/2.5/find",
+                    params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+            data = res.json()
+            cities = ["{} ({})".format(d['name'], d['sys']['country'])
+            for d in data['list']]
+            print("city:", cities)
+            try:
+                    city_id = data['list'][0]['id']
+                    city_id = city_id
+                    print("success")
+            except:
+                city_id = 524901
+                print("fail")
+
+            print('city_id=', city_id)
+            res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                            params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+            
+            data = res.json()
+            # print(data)
+            word = data["weather"][0]['description']
+            word = emojes.get(word, [])
+            word =  "".join(word)
+            if not word:
+                print("no")
+            
+            # await msg.delete()
+            print(s_city)
+            await interaction.response.send_message(f"температура: {data['main']['temp']}° \n \t \t \t погода: {data['weather'][0]['description']} {word} \n \t \t \t макс.температура: {data['main']['temp_min']}° \n \t \t \t мини.температура: {data['main']['temp_max']}°")
+            self.foo = True
+            self.stop()
+          
+        @discord.ui.button(label="weather for 5 days", 
+                       style=discord.ButtonStyle.blurple)
+        async def weatherfor5days(self, interaction: discord.Interaction, button: discord.ui.Button):
+            await interaction.response.send_message("sorry it still in development")
+            self.foo = True
+            self.stop()
+            return
+            # msg = await interaction.response.send_message("https://cdn.discordapp.com/emojis/1138172643867111595.gif?size=96&quality=lossless")    
+            emojes = {
+                "пасмурно": [":cloud:"],
+                "ясно": [":sunny:"],
+                "облачно с прояснениями": [":white_sun_cloud:"],
+                "дождь": [":cloud_rain:"],
+                "переменная облачность": [":white_sun_small_cloud:"]        
+
+            }
+            city_id = 0
+            res = requests.get("http://api.openweathermap.org/data/2.5/find",
+                        params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+            data = res.json()
+            cities = ["{} ({})".format(d['name'], d['sys']['country'])
+            for d in data['list']]
+            print("city:", cities)
+            try:
+                    city_id = data['list'][0]['id']
+                    city_id = city_id
+                    print("success")
+            except:
+                city_id = 524901
+                print("fail")
+                
+            print('city_id=', city_id)
+            res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                            params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+            data = res.json()   
+        #     word = data["weather"][0]['description']
+        #     word = emojes.get(word, [])
+        #     word =  "".join(word)
+        #     if not word:
+        #         print("no")
+            
+            # await msg.delete()
+            print(s_city)              
+            self.foo = True
+            self.stop()
+
+
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hi! I am a bot {bot.user}!')
-
-
-@bot.command(pass_context=True ,no_pm=True) 
-async def weather(ctx, s_city):
-        msg = await ctx.send("https://cdn.discordapp.com/emojis/1138172643867111595.gif?size=96&quality=lossless")
-        emojes = {
-              "пасмурно": [":cloud:"],
-              "ясно": [":sunny:"],
-              "облачно с прояснениями": [":white_sun_cloud:"],
-              "дождь": [":cloud_rain:"]
-
-        }
-        appid = "4874417f6b948d6308647178a019be2d"
-        city_id = 0
-        res = requests.get("http://api.openweathermap.org/data/2.5/find",
-                params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
-        data = res.json()
-        cities = ["{} ({})".format(d['name'], d['sys']['country'])
-        for d in data['list']]
-        print("city:", cities)
+async def weatherfor5days(ctx):
         try:
-                city_id = data['list'][0]['id']
-                city_id = city_id
-                print("secsei")
-        except:
-               city_id = 524901
-               print("fail")
-        print('city_id=', city_id)
-        res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                         params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
-        
-        data = res.json()
-        # print(data)
-        word = data["weather"][0]['description']
-        word = emojes.get(word, [])
-        word =  "".join(word)
-        if not word:
-              print("no")
-        
-        await msg.delete()
-        await ctx.send(f"температура: {data['main']['temp']}° \n \t \t \t погода: {data['weather'][0]['description']} {word} \n \t \t \t макс.температура: {data['main']['temp_min']}° \n \t \t \t мини.температура: {data['main']['temp_max']}°")
-        print("conditions:", data['weather'][0]['description'])
-        print("temp:", data['main']['temp'])
-        print("temp_min:", data['main']['temp_min'])
-        print("temp_max:", data['main']['temp_max'])
+                city_id = 524901
+                res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                           params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                data = res.json()
+                print(data['list'])
+                for i in data['list']:
+                        print( i['dt_txt'], '{0:+3.0f}'.format(i['main']['temp']), i['weather'][0]['description'] )
+        except Exception as e:
+                print("Exception (forecast):", e)
+        pass
 
+@bot.command()
+async def weather(ctx, s_city2):
+      global s_city
+      s_city = s_city2
+      view = SimpleView(timeout=15)
+#       button = discord.ui.Button(label="Clck me")
+#       view.add_item(button)
+      message = await ctx.send(view=view)
+      view.message = message
+      await view.wait()
+      await view.disable_all_items()
+
+      if view.foo is None:
+            print("timeOut")
+
+      elif view.foo is True:
+            print("ok")
+
+      else:
+            print("cancell")
 
 
 @weather.error
@@ -69,5 +160,4 @@ async def info_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
         await ctx.send('sorry you need to enter city name')
         
-
 bot.run(TOKEN)
